addfc472b95cb70eb63f020143aa0496c7e623aa
Linus Unnebäck <linus@folkdatorn.se>
2023-12-26 14:42:54 +0100

    🌹 Remove parenthesis from Clear enum variant

--- src/crypto.rs
+++ src/crypto.rs
@@ -6,7 +6,7 @@ use rand::random;
 use log::trace;
 
 pub enum Crypto {
-    Clear(),
+    Clear,
     AES { key: [u8; 16], iv: [u8; 16] },
 }
 
@@ -15,20 +15,20 @@ impl Crypto {
         if encrypt {
             Crypto::AES { key: random(), iv: random() }
         } else {
-            Crypto::Clear()
+            Crypto::Clear
         }
     }
 
     pub fn is_clear(&self) -> bool {
         match self {
-            Crypto::Clear() => true,
+            Crypto::Clear => true,
             _ => false,
         }
     }
 
     pub fn sdp(&self) -> String {
         match self {
-            Crypto::Clear() => String::from(""),
+            Crypto::Clear => String::from(""),
             Crypto::AES { key, iv } => {
                 let modules = BigNum::from_slice(&base64::decode("59dE8qLieItsH1WgjrcFRKj6eUWqi+bGLOX1HL3U3GhC/j0Qg90u3sG/1CUtwC5vOYvfDmFI6oSFXi5ELabWJmT2dKHzBJKa3k9ok+8t9ucRqMd6DZHJ2YCCLlDRKSKv6kDqnw4UwPdpOMXziC/AMj3Z/lUVX1G7WSHCAWKf1zNS1eLvqr+boEjXuBOitnZ/bDzPHrTOZz0Dew0uowxf/+sG+NCK3eQJVxqcaJ/vEHKIVd2M+5qL71yJQ+87X6oV3eaYvt3zWZYD6z5vYTcrtij2VZ9Zmni/UAaHqn9JdsBWLUEpVviYnhimNVvYFZeCXg/IdTQ+x4IRdiXNv5hEew==").unwrap()).unwrap();
                 let exponent = BigNum::from_slice(&base64::decode("AQAB").unwrap()).unwrap();
@@ -47,7 +47,7 @@ impl Crypto {
 
     pub fn encrypt(&self, data: Vec<u8>) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
         match self {
-            Crypto::Clear() => Ok(data),
+            Crypto::Clear => Ok(data),
             Crypto::AES { key, iv } => {
                 trace!("Encrypting {} bytes using AES 128-bit CBC", data.len());
                 Ok(encrypt(Cipher::aes_128_cbc(), key, Some(iv), &data)?)
